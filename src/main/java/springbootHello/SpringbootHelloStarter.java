5d87d51539e9bbccc5fda92a56cacf59f01b54c9
saigopianasuri <saigopikrishna.anasuri@gmail.com>
2019-06-04 22:25:46 -0400

    First commit

---
+++ src/main/java/springbootHello/SpringbootHelloStarter.java
@@ -0,0 +1,11 @@
+package springbootHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloStarter {
+    public static void main(String [] args){
+        SpringApplication.run(SpringbootHelloStarter.class);
+    }
+}
